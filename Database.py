91f0c65cd34546911bdd8a34fce8cbb5267dd588
armanghoreshi <armangh1377@gmail.com>
2018-12-13 17:48:39 +0330

    new branch

--- Database.py
+++ Database.py
@@ -1,24 +1,15 @@
 import pymysql
 import tkinter
-
+import Secretary as s
 db = pymysql.connect(host="localhost",
                      user="root",
-                     passwd="Serv3rforDB")
+                     passwd="")
 
 cursor = db.cursor()
 
-cursor.execute("USE Q4_2")
-cursor.execute("SELECT * FROM ComputerProduct")
-
-for i in cursor.fetchall():
-    print (i[0], i[1], i[2])
-
-root = tkinter.Tk()
-
-label = tkinter.Label(root, text="Hello World!", padx=10, pady=10)
-label.pack(
-)
-root.mainloop()
-
+cursor.execute("USE Hospital")
 
 db.close()
+
+a = s.foo()
+print(a)
